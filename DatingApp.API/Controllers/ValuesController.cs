ce414af6423d9cd5831b0b2ef036096ab1330836
Leonardo Darrain <leonardo.darrain@geyce.es>
2018-11-06 12:37:40 +0100

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var Values = await _context.Values.ToListAsync();
+            return Ok (Values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.Where(x=>x.Id == id).FirstOrDefaultAsync();
+            return Ok (value);
+        }
+
+        // [HttpGet]
+        // public IActionResult GetValues()
+        // {
+        //     var Values = _context.Values.ToList();
+        //     return Ok (Values);
+        // }
+
+        // GET api/values
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     return new string[] { "value1", "value3" };
+        // }
+
+        // GET api/values/5
+        // [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        // {
+        //     return "value";
+        // }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
